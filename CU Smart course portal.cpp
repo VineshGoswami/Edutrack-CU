3732a63ec850fe862ca8017a98e072671b5e33d4
VK Goswami <143211818+VKG2005@users.noreply.github.com>
2024-10-15 17:08:37 +0530

    create a abstract class

--- CU Smart course portal.cpp
+++ CU Smart course portal.cpp
@@ -1,5 +1,14 @@
 #include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
 using namespace std;
+class Course {
+public:
+	virtual void Print_details() = 0;
+};
+
+
 int main() {
 	return 0;
 }
