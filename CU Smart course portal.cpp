ede7d67a4f1cb6fd1905ff8bb2beafb7349ce05a
VK Goswami <143211818+VKG2005@users.noreply.github.com>
2024-10-15 17:21:47 +0530

    Created a derived class for offline course enrollment.

--- CU Smart course portal.cpp
+++ CU Smart course portal.cpp
@@ -5,10 +5,27 @@
 using namespace std;
 class Course {
 public:
-	virtual void Print_details() = 0;
+	virtual void print_Details() = 0;
 };
 
+class offline_Course : public Course {
+private:
+	string course_Name;
+	string course_Code;
+public:
+	offline_Course(string name, string code) {
+		course_Name = name;
+		course_Code = code;
+	}
+	void print_Details(){
+		cout << "Name of course: "<< course_Name <<"\n";
+		cout << "Code of the course is: "<< course_Code << "\n";
 
+	}
+};
 int main() {
+	offline_Course course1("c++", "23bcs103");
+	course1.print_Details();
+
 	return 0;
 }
