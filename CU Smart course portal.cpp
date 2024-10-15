2001049e03ea32fee6908559eac2813feb251de2
VK Goswami <143211818+VKG2005@users.noreply.github.com>
2024-10-15 17:37:22 +0530

    Created the online course derived class for online course enrollment.

--- CU Smart course portal.cpp
+++ CU Smart course portal.cpp
@@ -17,15 +17,38 @@ public:
 		course_Name = name;
 		course_Code = code;
 	}
-	void print_Details(){
-		cout << "Name of course: "<< course_Name <<"\n";
+	void print_Details() override{
+		cout << "Name of course is: "<< course_Name <<"\n";
 		cout << "Code of the course is: "<< course_Code << "\n";
 
 	}
 };
+
+class online_Course : public Course {
+private:
+	string course_Name;
+	string course_Code;
+public:
+	online_Course(string name, string code) {
+		course_Name = name;
+		course_Code = code;
+	}
+	void print_Details() override{
+		cout << "Name of te course is:" << course_Name << "\n";
+		cout << "Code of te course is:" << course_Code << "\n";
+	}
+
+
+};
 int main() {
-	offline_Course course1("c++", "23bcs103");
-	course1.print_Details();
+	{
+		offline_Course course1("c++", "23bcs103");
+		course1.print_Details();
+	}
+	{
+		online_Course course1("evs", "23bcs1998");
+		course1.print_Details();
+	}
 
 	return 0;
 }
