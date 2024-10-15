cf17d0f3bfa0799f1e3f3dd80ccd65a00462d3ed
VK Goswami <143211818+VKG2005@users.noreply.github.com>
2024-10-15 18:28:03 +0530

    Create student class for taking student dredential and pushback the registered course after registration

--- CU Smart course portal.cpp
+++ CU Smart course portal.cpp
@@ -2,6 +2,7 @@
 #include<vector>
 #include<string>
 #include<algorithm>
+#include<list>
 using namespace std;
 class Course {
 public:
@@ -38,17 +39,36 @@ public:
 		cout << "Code of te course is:" << course_Code << "\n";
 	}
 
+	class student {
+	private:
+		string Name;
+		string UID;
+		string Section;
+		string Password;
+		list< Course* > registered_course;
+	public:
+		student(string name, string uid, string section, string password) {
+			Name = name;
+			UID = uid;
+			Section = section;
+			Password = password;
+		}
+		void registered_course( Course* course) {
+			registered_course.push_back(course);
+		}
+		void print_reg_course() {
+			cout << "Registered course for :" << Name << "\n";
+			for (Course* course : registered_course) {
+				course->print_Details();
+			}
+		}
+
+	};
 
 };
 int main() {
-	{
-		offline_Course course1("c++", "23bcs103");
-		course1.print_Details();
-	}
-	{
-		online_Course course1("evs", "23bcs1998");
-		course1.print_Details();
-	}
+
+
 
 	return 0;
 }
